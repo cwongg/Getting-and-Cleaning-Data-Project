648246e4faa262d40e8342505c15f70e0b6dcc14
Conrad Wong <cwongg@msn.com>
2015-06-19 07:11:04 -0700

    Updated scripts

--- run_analysis.R
+++ run_analysis.R
@@ -25,31 +25,32 @@ data <- rbind(train_data, test_data)
 
 names <- read.table("R/Cleaning/features.txt")
 
-colnames(data) <- c("Subject", "Y", as.vector(names$V2))
+colnames(data) <- c("Subject", "Test Label", as.vector(names$V2))
 
 # Add the activity labels, by merging activity_labels with the data set 
 
 activity_labels <- read.table("R/Cleaning/activity_labels.txt")
-colnames(activity_labels) <- c("Y", "Activity")
+colnames(activity_labels) <- c("Test Label", "Activity")
 
-merged_data <- merge(data, activity_labels, by.x="Y", by.y="Y")
+merged_data <- merge(data, activity_labels, by.x="Test Label", by.y="Test Label")
 
 # Filters the Standard Deviation and Mean columns
 
 namesdt <- as.data.table(names)
 namessm <- namesdt[V2 %like% "mean()" | V2 %like% "std()"]
 namessm2 <- as.vector(namessm$V2)
-namessm2 <- c("Activity", "Y", "Subject", namessm2)
+namessm2 <- c("Activity", "Test Label", "Subject", namessm2)
 
 required_data <- merged_data[, colnames(merged_data) %in% namessm2]
 
+namessm2 <- as.vector(namessm$V2)
+melted_data <- melt(required_data, id=c("Activity", "Test Label", "Subject"), measure.vars = namessm2)
+colnames(melted_data) <- c("Activity", "Test Label", "Subject", "Variable", "Value")
 
-
-melted_data <- melt(required_data, id=c("Activity", "Y", "Subject"), measure.vars = namessm2)
-
-summary_data <- melted_data %>% group_by(Activity, Subject, variable) %>% summarize(mean(value))
+summary_data <- melted_data %>% group_by(Activity, Subject, Variable) %>% summarize(mean(Value, na.rm=TRUE))
 
 colnames(summary_data) <- c("Activity", "Subject", "Variable", "Average")
 
-write.table(summary_data, "R/Cleaning/Summary by Activity and Subject.txt", row.names=FALSE)
-write.table(required_data, "R/Cleaning/Standard Deviation and Mean data.txt", row.names=FALSE)
+write.table(summary_data, "R/Cleaning/Git/Getting-and-Cleaning-Data-Project/Summary by Activity and Subject.txt", row.names=FALSE)
+write.table(required_data, "R/Cleaning/Git/Getting-and-Cleaning-Data-Project/Standard Deviation and Mean data.txt", row.names=FALSE)
+write.table(melted_data, "R/Cleaning/Git/Getting-and-Cleaning-Data-Project/Melted Standard Deviation and Mean data.txt", row.names=FALSE)
